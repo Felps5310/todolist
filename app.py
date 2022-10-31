40136dacbe56a9630168fd7678e73d6b7c303bfa
Felps5310 <felipesiqueira102010@hotmail.com>
2022-10-31 21:26:44 +0000

    app web

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Minha primeira aplicação Wev")
+from flask import Flask
+
+app  = Flask("__name__")
+
+@app.route("/")
+def home():
+    return "Hello World"
+@app.route("/bye")
+def bye():
+    return "Bye"
+
+app.run(debug=True)
