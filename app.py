4631e60140fddca4ea6f0c8df04810afbf139d6d
Felps5310 <felipesiqueira102010@hotmail.com>
2022-11-18 20:50:48 +0000

    a

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app  = Flask(__name__)
 
 tasks = [
     {'name':'Estudar','finished':False},
-    {'name':'Dormir','finished':True}
+    {'name':'Dormir','finished':True},
+    {'name':'Comer','finished':True},
+    
 ]
 
 @app.route("/")
